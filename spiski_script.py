0d618279ac5f6f55bbad2845b420ec2db5395858
Наталья <natakhatina@gmail.com>
2018-10-24 20:16:35 +0300

    Определитель матрицы 2х2, 3x3, 4x4 доделанное

--- spiski_script.py
+++ spiski_script.py
@@ -8,26 +8,52 @@
 # ОПРЕДЕЛИТЕЛЬ МАТРИЦЫ:
 import copy
 
-L=[[1,2,3],
-   [4,5,6],
-   [7,8,9]]
+L=[[1,2,3,4],
+   [4,5,6,7],
+   [7,8,9,8],
+   [1,2,3,4]]
+
+# L=[[1,2,3],
+#    [4,5,6],
+#    [7,8,9]]
+
+# L=[[1,2],
+#    [3,4]]
 
 n=len(L) # кол-во строк и соотв-но размерность
-d=0
 
-def foo(M):
-    d2=M[0][0]*M[1][1]-M[0][1]*M[1][0]
+def foo(A):
+    d2=A[0][0]*A[1][1]-A[0][1]*A[1][0]
     return d2
 
-for i in range (0,n):
-    M = copy.deepcopy(L)
-    M.pop(0)
-    M[0].pop(i)
-    M[1].pop(i)
-    d2=foo(M)
-    d=d+(-1)**i*L[0][i]*d2
+def Hallo(M):
+    d3=0
+    for i in range (0,3):
+        A = copy.deepcopy(M)
+        A.pop(0)
+        for j in range (0,2):
+            A[j].pop(i)
+        d2=foo(A)
+        d3=d3+(-1)**i*M[0][i]*d2
+    return d3
+
+def Goodbuy (L):
+    d4=0
+    for i in range (0,4):
+        M=copy.deepcopy(L)
+        M.pop(0)
+        for j in range (0,3):
+            M[j].pop(i)
+        d3 = Hallo(M)
+        print(d3)
+        d4 = d4+(-1)**i*L[0][i]*d3
+    return d4
+
+if n==2:
+    d=foo(L);
+elif n==3:
+    d=Hallo(L)
+elif n==4:
+    d=Goodbuy(L)
 
 print('det =',d)
-
-# ddd=L[0][0]*(L[1][1]*L[2][2]-L[1][2]*L[2][1])-L[0][1]*( L[1][0]*L[2][2]-L[1][2]*L[2][0] )+L[0][2]*(L[1][0]*L[2][1]-L[1][1]*L[2][0])
-# print('ddd=',ddd)
